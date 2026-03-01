6cf5063dbe39dd7c828048334004294bcface4f3
say-Adeeb <adeebsyed98@gmail.com>
2026-03-01 09:30:54 +0530

    Add training script with evaluation and model saving

--- src/train.py
+++ src/train.py
@@ -1,1 +1,7 @@
-print(hello)
+import numpy as np  # Liniear algebra 
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+df = pd.read_csv("data/loan_data.csv")
+
+df = pd.read_csv("data/loan_data.csv")
+
